77abd616aa57a17a76e0b771e3572613e04a443e
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-04-05 18:48:29 +0530

    Create assign_cookies_remember.cpp

---
+++ Arrays/assign_cookies_remember.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/assign-cookies/
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int content=0;
+        int i=0,j=0;
+        while(i<g.size() && j<s.size()){
+            if(g[i]<=s[j]){
+                content++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return content;
+    }
+};
