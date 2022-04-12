303b303bd8bfff73113209b90ceda3f8c55002a9
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-04-12 13:35:32 +0530

    Create rotate_array_right_tricky.cpp

---
+++ Arrays/rotate_array_right_tricky.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/rotate-array/
+
+// Method 1: Using O(n)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        vector <int> res;
+        for(int i=n-k;i<n;i++)
+            res.push_back(nums[i]);
+        for(int i=0;i<n-k;i++)
+            res.push_back(nums[i]);
+        nums.clear();
+        nums=res;
+    }
+};
+
+
+// Method 2: Using Reverse Function
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
+
+
+// Method 3: Juggling Algorithm - Difficult
