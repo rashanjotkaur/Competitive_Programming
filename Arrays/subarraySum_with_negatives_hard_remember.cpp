a7621ca0380fa261f3518f617b8347afe41bf722
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-04-13 19:19:14 +0530

    Update subarraySum_with_negatives_hard_remember.cpp

--- Arrays/subarraySum_with_negatives_hard_remember.cpp
+++ Arrays/subarraySum_with_negatives_hard_remember.cpp
@@ -1,52 +1,53 @@
+// Print all the subarrays with sum=k
+// https://www.geeksforgeeks.org/find-subarray-with-given-sum-in-array-of-integers/
+// No practice question for this.
 
 // Method 1: O(n^2)
 
 class Solution {
 public:
-    void subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
+    void subArraySum(int arr[], int n, int k){
         int currSum;
         int count=0;
         for(int i=0;i<n;i++){
-            currSum=nums[i];
+            currSum=arr[i];
             for(int j=i+1;j<=n;j++){
-                // cout<<"currSum: "<<currSum<<endl;;
+                //cout<<"currSum: "<<currSum<<endl;
                 if(currSum==k)
                     cout<<"i: "<<i<<"  j: "<<j-1<<endl;
-                if(currSum>k || j==n)
-                    break;
-                currSum=currSum+nums[j];
+                if(j<n)
+                    currSum=currSum+arr[j];
             }
         }
     }
 };
 
 
+
 // Method 2: Important using unordered_map - O(n)
 
-void subarraySum(int arr[],int n,int sum){
-    int currSum=0,maxLen;
-    unordered_map <int,int> ump;
-    for(int i=0;i<n;i++){
-        currSum=currSum+arr[i];
-        if(currSum==sum){
-            cout<<"0"<<" "<<i<<endl;
-            return ;
+void subArraySum(int arr[], int n, int sum){
+        int currSum=0;
+	int maxLen;
+        unordered_map <int,int> ump;
+        for(int i=0;i<n;i++){
+            currSum=currSum+arr[i];
+            if(currSum==sum)
+                cout<<"0"<<" "<<i<<endl;
+            
+    	    //currSum=sum+x;
+            //0----i----j
+            //  x    sum
+            //ump[x]=i
+            //so, sum --> i+1 to j
+            //and i=ump[x], so sum --> ump[x]+1 to j
+            if(ump.find(currSum-sum)!=ump.end())
+                cout<<ump[currSum-sum]+1<<" "<<i<<endl;
+            
+    	    ump[currSum]=i;
         }
-	//currSum=sum+x;
-        //0----i----j
-        //  x    sum
-        //ump[x]=i
-        //so, sum --> i+1 to j
-        //and i=ump[x], so sum --> ump[x]+1 to j
-        if(ump.find(currSum-sum)!=ump.end()){
-            cout<<ump[currSum-sum]+1<<" "<<i<<endl;
-            return ;
-        }
-	ump[currSum]=i;
-    }
 }
 
 // Input
-// 5 -20
+// 5 -10
 // 10 2 -2 -20 10
