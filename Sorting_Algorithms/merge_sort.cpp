1449d6580fafce5cbf956fc8dfdb6b24054adf2d
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-04-25 16:47:37 +0530

    Update merge_sort.cpp

--- Sorting_Algorithms/merge_sort.cpp
+++ Sorting_Algorithms/merge_sort.cpp
@@ -72,3 +72,55 @@ public:
         return nums;
     }
 };
+
+
+
+
+// Without using extra space
+class Solution {
+public:
+    void merge(vector <int> &nums, int l, int mid, int r){
+        int n=r-l+1;
+        vector <int> temp(n);
+        int i=l,j=mid+1,k=0;
+        while(i<=mid && j<=r){
+            if(nums[i]<nums[j]){
+                temp[k]=nums[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k]=nums[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<=mid){
+            temp[k]=nums[i];
+            i++;
+            k++;
+        }
+        while(j<=r){
+            temp[k]=nums[j];
+            k++;
+            j++;
+        }
+        
+        for(int i=l;i<=r;i++)
+            nums[i]=temp[i-l];
+        
+    }
+    void mergeSort(vector <int> &nums, int l, int r){
+        if(r>l){
+            int mid=l+(r-l)/2;
+            mergeSort(nums,l,mid);
+            mergeSort(nums,mid+1,r);
+            merge(nums,l,mid,r);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        mergeSort(nums,0,n-1);
+        return nums;
+    }
+};
