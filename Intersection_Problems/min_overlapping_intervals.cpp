a9192c5bd419f09de7fb7812a92ef78aabcb39a4
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-04-26 07:56:40 +0530

    Create min_overlapping_intervals.cpp

---
+++ Intersection_Problems/min_overlapping_intervals.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/non-overlapping-intervals/
+
+class Solution {
+public:
+    bool static sortbysec(const pair<int,int> &a, const pair<int,int> &b){
+        if(a.second==b.second)
+            return a.first<b.first;
+       return a.second<b.second;
+    }
+    int eraseOverlapIntervals(vector <vector<int>> &intervals) {
+        int n=intervals.size();
+        vector <pair<int,int>> vec;
+        for(int i=0;i<n;i++)
+            vec.push_back(make_pair(intervals[i][0],intervals[i][1]));
+        sort(vec.begin(),vec.end(),sortbysec);
+        
+        int nonOverlapCount=1;
+        int l1,r1,r0;
+        r0=vec[0].second;
+        for(int i=1;i<n;i++){
+            l1=vec[i].first;
+            r1=vec[i].second;
+            if(l1>=r0){
+                nonOverlapCount++;
+                r0=r1;
+            }
+        }
+        return n-nonOverlapCount;
+    }
+};
