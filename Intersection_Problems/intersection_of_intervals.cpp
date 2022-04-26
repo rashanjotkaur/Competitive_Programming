611ff568d1ecb02428de5fa86bc21b46387b7ccb
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-04-26 08:34:54 +0530

    Create intersection_of_intervals.cpp

---
+++ Intersection_Problems/intersection_of_intervals.cpp
@@ -0,0 +1,55 @@
+// https://www.geeksforgeeks.org/find-intersection-of-all-intervals/
+
+// Input
+// 4
+// 1 6
+// 2 8
+// 3 10
+// 5 8
+
+// 2
+// 1 6
+// 8 18
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b){
+    if(a.second==b.second)
+        return a.first<b.first;
+    return a.second<b.second;
+}
+
+void checkNonOverlappingIntervals(vector <pair<int,int>> intervals, int n){
+    sort(intervals.begin(),intervals.end(),sortbysec);
+    int lmax=intervals[0].first;
+    int rmin=intervals[0].second;
+    int r0=intervals[0].second;
+    for(int i=1;i<n;i++){
+        int l1=intervals[i].first;
+        int r1=intervals[i].second;
+        if(l1>r0){
+            cout<<"-1"<<endl;
+            return;
+        }
+        else{
+            lmax=max(lmax,l1);
+            rmin=min(rmin,r1);
+            r0=r1;
+        }
+    }
+    cout<<lmax<<" "<<rmin<<endl;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	vector <pair<int,int>> intervals;
+	int x,y;
+	for(int i=0;i<n;i++){
+	    cin>>x>>y;
+	    intervals.push_back(make_pair(x,y));
+	}
+	checkNonOverlappingIntervals(intervals,n);
+	return 0;
+}
