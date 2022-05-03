7bbee3bc62db925d1ed8e09a7fe47cebd6023f18
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-03 10:44:23 +0530

    Update implement_stack_usingPriorityQueue.cpp

--- Stack/implement_stack_usingPriorityQueue.cpp
+++ Stack/implement_stack_usingPriorityQueue.cpp
@@ -1,1 +1,33 @@
+// https://www.geeksforgeeks.org/implement-stack-using-priority-queue-or-heap/
 
+class Stack{
+	public:
+	    typedef pair<int,int> pi;
+      priority_queue <pi> pq; //By default greater
+    	int count=0;
+    	
+    	void push(int x){
+        	count++;
+        	pq.push(make_pair(count,x));
+        }
+        
+        int pop(){
+        	if(pq.empty())
+        	    return -1;
+        	count--;
+        	pq.pop();
+        }
+        
+        int top(){
+            if(pq.empty())
+                return -1;
+        	pi temp=pq.top();
+        	return temp.second;
+        }
+        
+        bool isEmpty(){
+            if(pq.empty())
+        	    return true;
+        	return false;
+        }
+};
