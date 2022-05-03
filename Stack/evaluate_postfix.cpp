1d2c9a5b51bede961cc277f67febb2ac6aa816a8
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-03 11:28:05 +0530

    Update evaluate_postfix.cpp

--- Stack/evaluate_postfix.cpp
+++ Stack/evaluate_postfix.cpp
@@ -1,1 +1,30 @@
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
 
+class Solution {
+public:
+    int evalRPN(vector <string> &token) {
+        stack <int> s;
+        int n=token.size();
+        for(int i=0;i<n;i++){
+            if(token[i]=="*" || token[i]=="/" || token[i]=="+" || token[i]=="-"){
+                int x=s.top();
+                s.pop();
+                int y=s.top();
+                s.pop();
+                if(token[i]=="*")
+                    s.push(y*x);
+                else if(token[i]=="/")
+                    s.push(y/x);
+                else if(token[i]=="-")
+                    s.push(y-x);
+                else if(token[i]=="+")
+                    s.push(y+x);
+            }
+            else{
+                int temp=stoi(token[i]);
+                s.push(temp);
+            }
+        }
+        return s.top();
+    }
+};
