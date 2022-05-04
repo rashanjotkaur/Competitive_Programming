a6814241f0a31542794a244a3c5ddbf90cb16316
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-04 14:52:52 +0530

    Create bubble_sort.cpp

---
+++ Sorting_Algorithms/bubble_sort.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/sort-colors/
+
+class Solution {
+public:
+    void sortColors(vector<int> &nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            bool swapped=false;
+            for(int j=0;j<n-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                    swapped=true;
+                }
+            }
+            if(!swapped)
+                break;
+        }
+    }
+};
