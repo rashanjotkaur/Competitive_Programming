777d6525171bd89dc19548f663a123dde688cdd4
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-04 15:19:57 +0530

    Create insertion_sort.cpp

---
+++ Sorting_Algorithms/insertion_sort.cpp
@@ -0,0 +1,41 @@
+// https://practice.geeksforgeeks.org/problems/insertion-sort/1/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector <int> &nums, int n){
+      for(int i=1;i<n;i++){
+          int key=nums[i];
+          int j=i-1;
+          while(j>=0 && nums[j]>key){
+            nums[j+1]=nums[j];
+            j--;
+          }
+          nums[j+1]=key;
+      }
+}
+
+int main() {
+      int t;
+      cin>>t;
+      while(t--){
+          int n;
+          cin>>n;
+          vector <int> nums(n);
+          for(int i=0;i<n;i++)
+            cin>>nums[i];
+          insertionSort(nums,n);
+          for(int i=0;i<n;i++)
+            cout<<nums[i]<<" ";
+          cout<<endl;
+      }
+      return 0;
+}
+
+// Input:
+// 1
+// 7
+// 10 80 30 90 40 50 70
+  
+// Output:
+// 10 30 40 50 70 80 90 
