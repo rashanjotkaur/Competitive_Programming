5a5d5a697935ce3a83234d8a617dbb1b7180aa8b
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-05 13:23:21 +0530

    Update 5. mini_subsetSum_diff.cpp

--- Dynamic_Programming/0-1 KnapSack/5. mini_subsetSum_diff.cpp
+++ Dynamic_Programming/0-1 KnapSack/5. mini_subsetSum_diff.cpp
@@ -31,4 +31,36 @@ public:
 	  } 
 
 
-// 
+// Method 2: Instead of making dp till sum, we can make it till s2 only 
+// s1+s2=sum
+// s1-s2=x
+// 2*s2=sum-x
+// x=sum-2*s2 // We have to return this -> as this is the minimum difference, when s2 is maximum.
+// x>=0
+// Hence, sum-2*s2>=0
+// s2<=sum/2
+public:
+    	typedef long long int ll;
+	int minDifference(int arr[], int n)  { 
+	    ll sum=0;
+	    for(ll i=0;i<n;i++)
+	        sum+=arr[i];
+	    ll s2=sum/2;
+	    bool dp[n+1][s2+1];
+	    for(ll i=0;i<=n;i++)
+	        dp[i][0]=true;
+	    for(ll j=1;j<=s2;j++)
+	        dp[0][j]=false;
+	    for(ll i=1;i<=n;i++){
+	        for(ll j=1;j<=s2;j++){
+	            if(arr[i-1]<=j)
+	                dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+	            else
+	                dp[i][j]=dp[i-1][j];
+	        }
+	    }
+	    for(ll j=s2;j>=0;j--){
+	        if(dp[n][j]==true)
+	            return sum-2*j;
+	    }
+	} 
