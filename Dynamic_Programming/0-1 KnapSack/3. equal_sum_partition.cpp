9880213c7a3b3506877be6d5f15740974e4d6539
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-05 07:44:34 +0530

    Create 3. equal_sum_partition.cpp

---
+++ Dynamic_Programming/0-1 KnapSack/3. equal_sum_partition.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/partition-equal-subset-sum/
+
+// Similar to Subset Sum Problem
+// Here, We have to divide array into two partitions such that there sum is equal.
+// Partition 1 Sum == Partition 2 Sum
+// Step 1: Find the sum of array => Sum
+// Step 2: If Sum is ODD, then it's not possible. ---> Return FALSE
+// Step 3: If Sum is EVEN, then it's possible. ---> Now Check if there exists subset in the array with sum = Sum/2;
+//            Hence, this is a Subset Sum Problem.
+
+class Solution {
+public:
+    bool subsetSum(vector <int> nums, int n, int sum){
+        bool dp[n+1][sum+1];
+        for(int i=0;i<=n;i++)
+            dp[i][0]=true;
+        for(int j=1;j<=sum;j++)
+            dp[0][j]=false;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(nums[i-1]<=j)
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else
+                    dp[i][j]=dp[i-1][j];
+                
+            }
+        }
+        return dp[n][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=nums[i];
+        if(sum%2==0)
+            return subsetSum(nums,n,sum/2);
+        return false;
+    }
+};
