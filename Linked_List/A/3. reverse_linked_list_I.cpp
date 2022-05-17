8a030a28c512992727d37ab6ca05406016ba4b32
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-17 17:07:41 +0530

    Update 3. reverse_linked_list_I.cpp

--- Linked_List/A/3. reverse_linked_list_I.cpp
+++ Linked_List/A/3. reverse_linked_list_I.cpp
@@ -2,6 +2,7 @@
 
 // Input: 1 2 3 4 5
 // Output: 3 2 1 5 4
+// Method 1 - Without using extra space
 class Solution{
     public:
     struct node *reverse(struct node *head, int k){ 
@@ -26,4 +27,42 @@ class Solution{
         
         return prev;
     }
+};
+
+
+// Method 2 - Using Stack
+class Solution{
+    public:
+        struct node *reverse(struct node *head, int k){ 
+            stack <struct node*> s; 
+            
+            struct node *curr = head;
+            struct node *prev = NULL;
+            struct node *next = NULL;
+         
+            while (curr != NULL){
+                int i = 0;
+                while (curr != NULL && i < k){
+                    s.push(curr);
+                    curr = curr->next;
+                    i++;
+                }
+         
+                while (s.size() > 0){
+                    if (prev == NULL){
+                        prev = s.top();
+                        head = prev;
+                        s.pop();
+                    }
+                    else{
+                        prev->next = s.top();
+                        prev = prev->next;
+                        s.pop();
+                    }
+                }
+            }
+         
+            prev->next = NULL;
+            return head;
+        }
 };
