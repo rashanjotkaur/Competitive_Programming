71b33ddca621ed93b1bb86614e4028a90e5abeab
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-17 16:53:53 +0530

    Create 3. reverse_linked_list_I.cpp

---
+++ Linked_List/A/3. reverse_linked_list_I.cpp
@@ -0,0 +1,29 @@
+// https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1#
+
+// Input: 1 2 3 4 5
+// Output: 3 2 1 5 4
+class Solution{
+    public:
+    struct node *reverse(struct node *head, int k){ 
+        if(head==NULL)
+            return head;
+        
+        struct node *curr=head;
+        struct node *next=NULL;
+        struct node *prev=NULL;
+        
+        int i=0;
+        while(curr && i<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            i++;
+        }
+        
+        if(next)
+            head->next=reverse(next,k);
+        
+        return prev;
+    }
+};
