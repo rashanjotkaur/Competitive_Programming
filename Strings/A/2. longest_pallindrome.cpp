4e347e7e40e0439562402164e256d55cec5141f3
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-18 19:43:12 +0530

    Update 2. longest_pallindrome.cpp

--- Strings/A/2. longest_pallindrome.cpp
+++ Strings/A/2. longest_pallindrome.cpp
@@ -1,1 +1,22 @@
+// https://leetcode.com/problems/longest-palindrome/
 
+// Tricky Remember
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n=s.size();
+        unordered_map <char,int> ump;
+        for(int i=0;i<n;i++)
+            ump[s[i]]++;
+        int len=0;
+        for(auto x:ump){
+            int fre=x.second;
+            if(fre%2==0)
+                len=len+fre;  
+            else
+                len=len+(fre/2)*2;
+        }
+        return len<n?len+1:len;
+    }
+};
