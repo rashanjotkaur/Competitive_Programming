d8ba3a5b9b8c8cc621602c32351ea9cd7933d5de
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-19 08:59:33 +0530

    Update 0. is_pallindrome.cpp

--- Linked_List/B/0. is_pallindrome.cpp
+++ Linked_List/B/0. is_pallindrome.cpp
@@ -1,1 +1,43 @@
+// https://leetcode.com/problems/palindrome-linked-list/
 
+class Solution {
+public:
+    ListNode* reverse(ListNode *head){
+        if(head==NULL)
+            return NULL;
+        
+        ListNode *curr=head;
+        ListNode *next=NULL;
+        ListNode *prev=NULL;
+        
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;       
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL)
+            return false;
+        
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        ListNode *rev=reverse(slow);
+        ListNode* forw=head;
+        while(rev){
+            if(rev->val!=forw->val)
+                return false;
+            
+            rev=rev->next;
+            forw=forw->next;
+        }
+        return true;
+    }
+};
