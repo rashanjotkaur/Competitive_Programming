2eb99cc8818da838aa22c7e6a6389731090b9b0c
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-19 17:31:41 +0530

    Update 6. intersection_of_linked_lists_II.cpp

--- Linked_List/B/6. intersection_of_linked_lists_II.cpp
+++ Linked_List/B/6. intersection_of_linked_lists_II.cpp
@@ -1,1 +1,34 @@
+// https://practice.geeksforgeeks.org/problems/intersection-of-two-linked-list/1/#
 
+class Solution{
+  public:
+    Node* newNode(int x){
+        Node *temp=new Node(x);
+        return temp;
+    }
+    Node* insert(Node *head, int x){
+        if(head==NULL){
+            head=newNode(x);
+            return head;
+        }
+        Node *temp=head;
+        while(temp->next)
+            temp=temp->next;
+        temp->next=newNode(x);
+        return head;
+    }
+    Node* findIntersection(Node* a, Node* b){
+        unordered_map <int,bool> ump;
+        while(b){
+            ump[b->data]=true;
+            b=b->next;
+        }
+        Node *res=NULL;
+        while(a){
+            if(ump[a->data])
+                res=insert(res,a->data);
+            a=a->next;    
+        }
+        return res;
+    }
+};
