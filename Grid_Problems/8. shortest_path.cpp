56dee03e677eea2df2601185866737df47a42e94
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2022-05-21 07:13:06 +0530

    Update 8. shortest_path.cpp

--- Grid_Problems/8. shortest_path.cpp
+++ Grid_Problems/8. shortest_path.cpp
@@ -1,6 +1,29 @@
 // https://leetcode.com/problems/minimum-path-sum/
 
-// Method 1
+// Method 1 - Space Complexity less than Method 2
+// Don't use extra dp as We used in Method 2
+class Solution {
+public:
+    int minPathSum(vector <vector<int>> &grid) {
+        int row=grid.size();
+        if(row==0)
+            return 0;
+        int col=grid[0].size();
+        for(int i=1;i<row;i++)
+            grid[i][0]+=grid[i-1][0];
+        
+        for(int j=1;j<col;j++)
+            grid[0][j]+=grid[0][j-1];
+        
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++)
+                grid[i][j]=min(grid[i][j-1],grid[i-1][j])+grid[i][j];
+        }
+        return grid[row-1][col-1];
+    }
+};
+
+// Method 2
 class Solution {
 public:
     int minPathSum(vector <vector<int>> &grid) {
@@ -29,7 +52,7 @@ public:
     }
 };
 
-// Method 2
+// Method 3 - Most optimal
 class Solution {
 public:
     int minPathSum(vector <vector<int>> &path) {
