b6b671786c06c6aac636ba912c39c145f7305e85
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-10 18:42:52 +0530

    Update 4. union_of_linked_list.cpp

--- Linked_List/B/4. union_of_linked_list.cpp
+++ Linked_List/B/4. union_of_linked_list.cpp
@@ -130,3 +130,111 @@ Node* unionFun(Node *a, Node *b){
     }
     return res;
 }
+
+
+
+// Method 3 - Here also we will sort the linked lists but will not remove the duplicates while sorting.
+// We will remove the duplicates while adding into the linked lists.
+
+// Func 1 - unionFun
+struct Node* makeUnion(struct Node* a, struct Node* b){
+    mergeSort(a);
+    mergeSort(b);
+    
+    Node *res=newNode(0);
+    Node *temp=res;
+    while(a && b){
+        if(a->data<b->data){
+            if(temp->data!=a->data){
+                temp->next=newNode(a->data);
+                temp=temp->next;
+            }
+            a=a->next;
+        }
+        else if(b->data<a->data){
+            if(temp->data!=b->data){
+                temp->next=newNode(b->data);
+                temp=temp->next;
+            }
+            b=b->next;
+        }
+        else if(a->data==b->data){
+            if(temp->data!=a->data){
+                temp->next=newNode(a->data);
+                temp=temp->next;
+            }
+            a=a->next;
+            b=b->next;
+        }
+    }
+    while(a){
+        if(temp->data!=a->data){
+            temp->next=newNode(a->data);
+            temp=temp->next;
+        }
+        a=a->next; 
+    }
+    while(b){
+        if(temp->data!=b->data){
+            temp->next=newNode(b->data);
+            temp=temp->next;
+        }
+        b=b->next;
+    }
+    return res->next;
+}
+
+// Func 2 - mergeSort
+Node* mergeSort(Node* &head){
+    if(head==NULL || head->next==NULL)
+        return head;
+        
+    Node *a=NULL;
+    Node *b=NULL;
+    split(head,a,b);
+    
+    mergeSort(a);
+    mergeSort(b);
+    
+    head=merge(a,b);
+    return head;
+}
+
+// Func 3 - split
+void split(Node *head, Node *&a, Node *&b){
+    Node *slow=head;
+    Node *fast=head->next;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    a=head;
+    b=slow->next;
+    slow->next=NULL;
+}
+
+// Func 4 - merge
+Node* merge(Node *a, Node *b){
+    if(a==NULL)
+        return b;
+    if(b==NULL)
+        return a;
+    Node *res=NULL;
+    if(a->data<b->data){
+        res=a;
+        res->next=merge(a->next,b);
+    }
+    else{
+        res=b;
+        res->next=merge(a,b->next);
+    }
+    return res;
+}
+
+// Func 5 - newNode 
+Node* newNode(int x){
+    Node *temp=new Node(x);
+    temp->data=x;
+    temp->next=NULL;
+    return temp;
+}
