e9bc96e6883c1b98e44904b193e34cecc470cd45
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-10 12:49:43 +0530

    Update 5b. intersection_of_two_linked_lists_in_sorted_order.cpp

--- Linked_List/B/5b. intersection_of_two_linked_lists_in_sorted_order.cpp
+++ Linked_List/B/5b. intersection_of_two_linked_lists_in_sorted_order.cpp
@@ -1,130 +1,26 @@
 // Answer will be in the sorted form as we are using merge sort
 
-// Method 1
-Node* newNode(int x){
-    Node *temp=new Node(x);
-    temp->data=x;
-    temp->next=NULL;
-    return temp;
-}
-void insert(Node* &head,int x){
-    if(head==NULL){
-        head=newNode(x);
-        return ;
-    }
-    Node *temp=head;
-    while(temp->next)
-        temp=temp->next;
-    temp->next=newNode(x);
-}
-Node* findIntersection(Node* head1, Node* head2){
-    if(head1==NULL)
-        return head2;
-    if(head2==NULL)
-        return head1;
-    Node *head=NULL;
-    while(head1 && head2){
-        if(head1->data==head2->data){
-            insert(head,head1->data);
-            head1=head1->next;
-            head2=head2->next;
-        }
-        else if(head1->data<head2->data)
-            head1=head1->next;
-        else
-            head2=head2->next;
-    }
-    return head;
-}
+// Steps:
+// 1. Sort the both linked lists -> sort head1 && sort head2 using merge sort
+// 2. Then use the interesection function.
 
-
-
-// Main Function
-Node* findIntersection(Node* a, Node* b){
-        if(a==NULL)
-            return a;
-        if(b==NULL)
-            return b;
-        
-        mergeSort(a);
-        mergeSort(b);
-        
-        return intersection(a,b);
-}
-
-// Function - 1
-Node* mergeSort(Node *&head){
-        if(head==NULL || head->next==NULL)
-            return head;
-        
-        Node *a=NULL;
-        Node *b=NULL;
-        split(head,a,b);
-        
-        mergeSort(a);
-        mergeSort(b);
-        head=mergeSortUtil(a,b);
-        return head;
-}
-
-// Function - 2
-void split(Node *head, Node *&a, Node *&b){
-        Node *slow=head;
-        Node *fast=head->next;
-        while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        a=head;
-        b=slow->next;
-        slow->next=NULL;
-}
-
-// Function - 3
-Node* mergeSortUtil(Node *a, Node *b){
-        Node *res=NULL;
-        if(a==NULL)
-            return b;
-        if(b==NULL)
-            return a;
-        if(a->data<b->data){
-            res=a;
-            res->next=mergeSortUtil(a->next,b);
-        }
-        else if(a->data>b->data){
-            res=b;
-            res->next=mergeSortUtil(a,b->next);
-        }
-        else{  // if equal
-            res=a;
-            res->next=mergeSortUtil(a->next,b->next);
-        }
-        return res;
-}
-
-// Function - 4
-Node *intersection(Node *a,Node *b){
-        if(a==NULL || b==NULL)
-            return NULL;
-        Node *res=NULL;
-        if(a->data<b->data)
-            res=intersection(a->next,b);
-        else if(b->data<a->data)
-            res=intersection(a,b->next);
-        else if(a->data==b->data){
-            res=a;
-            res->next=intersection(a->next,b->next);
-        }
-        return res;
-}
-
+// Input
+// Testcases: 2
+// n1=5
+// 1 2 3 6 4
+// n2=3
+// 8 4 6
+// n1=4
+// 10 20 40 50
+// n2=2
+// 15 40
 
 #include<bits/stdc++.h>
 using namespace std;
 
 struct Node{
-    int x;
-    Node *temp;
+    int data;
+    Node *next;
 };
 
 Node* newNode(int x){
@@ -134,7 +30,7 @@ Node* newNode(int x){
     return temp;
 }
 
-void insert(Node *head,int x){
+void insert(Node *&head,int x){
     if(head==NULL){
         head=newNode(x);
         return ;
@@ -150,6 +46,7 @@ void print(Node *head){
         cout<<head->data<<" ";
         head=head->next;
     }
+    cout<<endl;
 }
 
 Node* intersection(Node *head1,Node *head2){
@@ -168,6 +65,7 @@ Node* intersection(Node *head1,Node *head2){
             head1=head1->next;
         else
             head2=head2->next;
+    }
     return head;
 }
 
@@ -232,7 +130,9 @@ int main(){
             insert(head2,x);
         }
         mergeSort(head1);
+        print(head1);
         mergeSort(head2);
+        print(head2);
         Node *head=NULL;
         head=intersection(head1,head2);
         print(head);
@@ -241,16 +141,21 @@ int main(){
 }
 
 
-
-
-
-
-
-
-
-
-
-
+// Intersection Method - Using Recursion
+Node *intersection(Node *a,Node *b){
+        if(a==NULL || b==NULL)
+            return NULL;
+        Node *res=NULL;
+        if(a->data<b->data)
+            res=intersection(a->next,b);
+        else if(b->data<a->data)
+            res=intersection(a,b->next);
+        else if(a->data==b->data){
+            res=a;
+            res->next=intersection(a->next,b->next);
+        }
+        return res;
+}
 
 
 
