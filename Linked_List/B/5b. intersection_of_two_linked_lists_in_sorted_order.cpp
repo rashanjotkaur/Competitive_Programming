abb71f658ba52b03ae8c192e29b4f57d0c0d4b57
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-10 09:39:09 +0530

    Update 5b. intersection_of_two_linked_lists_in_sorted_order.cpp

--- Linked_List/B/5b. intersection_of_two_linked_lists_in_sorted_order.cpp
+++ Linked_List/B/5b. intersection_of_two_linked_lists_in_sorted_order.cpp
@@ -1,5 +1,43 @@
 // Answer will be in the sorted form as we are using merge sort
 
+// Method 1
+Node* newNode(int x){
+    Node *temp=new Node(x);
+    temp->data=x;
+    temp->next=NULL;
+    return temp;
+}
+void insert(Node* &head,int x){
+    if(head==NULL){
+        head=newNode(x);
+        return ;
+    }
+    Node *temp=head;
+    while(temp->next)
+        temp=temp->next;
+    temp->next=newNode(x);
+}
+Node* findIntersection(Node* head1, Node* head2){
+    if(head1==NULL)
+        return head2;
+    if(head2==NULL)
+        return head1;
+    Node *head=NULL;
+    while(head1 && head2){
+        if(head1->data==head2->data){
+            insert(head,head1->data);
+            head1=head1->next;
+            head2=head2->next;
+        }
+        else if(head1->data<head2->data)
+            head1=head1->next;
+        else
+            head2=head2->next;
+    }
+    return head;
+}
+
+
 
 // Main Function
 Node* findIntersection(Node* a, Node* b){
