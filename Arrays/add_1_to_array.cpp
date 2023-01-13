6b9ad2d77951a3a20db854ae93b101ba8eb6e123
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-14 02:00:07 +0530

    Update add_1_to_array.cpp

--- Arrays/add_1_to_array.cpp
+++ Arrays/add_1_to_array.cpp
@@ -6,6 +6,7 @@
 // Input: digits = [9,9,9] + 1
 // Output: [1,0,0,0]
 
+// Method 1:
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
@@ -21,6 +22,37 @@ public:
         else
             digits[r]++;
         
+        return digits;
+    }
+};
+
+
+// Method 2: By Reversing the Vector
+class Solution {
+public:
+    vector <int> plusOne(vector <int> &digits) {
+        int n=digits.size();
+        int carry=0;
+        reverse(digits.begin(),digits.end());
+        digits[0]++;
+        if(digits[0]>=10){
+            digits[0]%=10;
+            carry=1;
+        }
+        for(int i=1;i<n;i++){
+            digits[i]+=carry;
+            if(digits[i]>=10){
+                digits[i]%=10;
+                carry=1;
+            }
+            else{
+                carry=0;
+                break;
+            }
+        }
+        if(carry)
+            digits.push_back(carry);
+        reverse(digits.begin(),digits.end());
         return digits;
     }
 };
