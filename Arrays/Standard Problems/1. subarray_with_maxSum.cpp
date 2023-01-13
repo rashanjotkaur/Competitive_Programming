3c4a5a1c281d0a289e292ebf582266071ebc5923
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-13 22:15:28 +0530

    Update and rename Arrays/subarray_with_maxSum.cpp to Arrays/Standard Problems/1. subarray_with_maxSum.cpp

--- Arrays/Standard Problems/1. subarray_with_maxSum.cpp
+++ Arrays/Standard Problems/1. subarray_with_maxSum.cpp
@@ -23,3 +23,22 @@ public:
         return max_so_far;
     }
 };
+
+
+// In Simple Words
+class Solution {
+public:
+    int maxSubArray(vector <int> &nums) {
+        int n=nums.size();
+        int currSum=0;
+        int maxSum=INT_MIN;
+        for(int i=0;i<n;i++){
+            currSum+=nums[i];
+            if(currSum>maxSum)
+                maxSum=currSum;
+            if(currSum<0)
+                currSum=0;
+        }
+        return maxSum;
+    }
+};
