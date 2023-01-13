0cf35f66fd065c7a65ab285abd65f7c0c9901552
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-14 01:38:18 +0530

    Update 1. duplicate_within_K_dis.cpp

--- Arrays/Duplicate Element Problems/1. duplicate_within_K_dis.cpp
+++ Arrays/Duplicate Element Problems/1. duplicate_within_K_dis.cpp
@@ -84,3 +84,22 @@ bool duplicateWithinK(vector <int> vec, int k){
     }
     return false;
 }
+
+
+// OR
+
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map <int,int> ump;
+        for(int i=0;i<nums.size();i++){
+            if(ump[nums[i]]){
+                int j=ump[nums[i]];
+                j--;
+                int diff=abs(i-j);
+                if(diff<=k)
+                    return true;
+            }
+            ump[nums[i]]=i+1;
+        }
+        return false;
+}
