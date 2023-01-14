669775ceca0b4655ed24a146861cc298e0a16d00
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-14 14:02:43 +0530

    Update 2. search_in_rotated_sorted_array.cpp

--- Arrays/Standard Problems/2. search_in_rotated_sorted_array.cpp
+++ Arrays/Standard Problems/2. search_in_rotated_sorted_array.cpp
@@ -1,12 +1,13 @@
 // Search in Rotated Sorted Array
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
+// Method 1: By Finding Pivot
 // Func 1: 
 int search(vector <int> &nums, int target) {
         int n=nums.size();
         int pivot=findPivot(nums,0,n-1);
         cout<<"pivot: "<<pivot<<endl;
-        int idx=-1;
+        int idx;
         if(pivot==0)
             idx=binarySearch(nums,0,n-1,target);
         else if(nums[pivot]==target)
@@ -49,3 +50,27 @@ int binarySearch(vector <int> nums, int low, int high, int key){
         return -1;
 }
     
+
+// Method 2: Without finding Pivot
+int search(vector <int> &nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[low]<=nums[mid]) {
+                if(target>=nums[low] && target<=nums[mid])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else{
+                if(target>=nums[mid] && target<=nums[high])
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return -1;
+}
