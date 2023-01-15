79a66ac326314af10fc6dc52fe3d1b0585c9177f
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-15 20:10:52 +0530

    Update majority_element.cpp

--- Arrays/majority_element.cpp
+++ Arrays/majority_element.cpp
@@ -1,81 +1,66 @@
-// Method 1: Using hash -- Unordered_map
-#include <bits/stdc++.h>
-using namespace std;
+// https://leetcode.com/problems/majority-element/
 
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    int n,i,x;
-	    cin>>n;
-	    int arr[n];
-	    unordered_map <int,int> m;
-	    for(i=0;i<n;i++){
-	        cin>>arr[i];
-	        m[arr[i]]++;
-	    }
-	    int maxi=INT_MIN;
-	    int flag=0;
-	    int ans;
-	    for(i=0;i<n;i++){
-	       // cout<<m[arr[i]]<<" ";
-	        if(m[arr[i]]>maxi){
-	            maxi=m[arr[i]];
-	            m[arr[i]]=0;
-	            ans=arr[i];
-	        }
-	    }
-	    if(maxi>n/2){
-	        cout<<ans<<endl;
-	    }
-	    else{
-	        cout<<"-1"<<endl;
-	    }
-	}
-	return 0;
+// Method 1: Preferred - Moore's Voting Algorithm
+// Func 1: 
+int majorityElement(vector <int> &nums) {
+        int n=nums.size();
+        int candidate_key=mooreVoting(nums,n);
+        return verify(nums,n,candidate_key);
 }
 
-// Method 2: Moore's Voting Algorithm - Time Complexity: O(n) & Space Complexity: O(1)
-#include <bits/stdc++.h>
-using namespace std;
-int moore_voting(int arr[],int n){
-    int count=1;
-    int maj_idx=0;
-    for(int i=0;i<n-1;i++){
-        if(arr[i]==arr[i+1])
-            count++;
-        else
-            count--;
-        if(count==0){
-            maj_idx=i+1;
-            count=1;
+// Func 2: 
+int mooreVoting(vector <int> nums, int n){
+        int maj_idx=0;
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[maj_idx]==nums[i])
+                count++;
+            else
+                count--;
+            if(count==0) {
+                maj_idx=i;
+                count=1;
+            }
         }
-    }
-    return arr[maj_idx];
+        return nums[maj_idx];
 }
-int verify(int arr[],int n,int candidate_key){
-    int fre=0;
-    for(int i=0;i<n;i++){
-	   if(arr[i]==candidate_key){
-	       fre++;
-	   }
-	   if(fre>n/2)
-	       return candidate_key;
-    }
-    return -1;
+
+// Func 3: 
+int verify(vector <int> nums, int n, int candidate_key){
+        int fre=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate_key)
+                fre++;
+            if(fre>n/2)
+                return candidate_key;
+        }
+        return -1;
 }
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    int n,i,x;
-	    cin>>n;
-	    int arr[n];
-	    for(i=0;i<n;i++){
-	        cin>>arr[i];
-	    }
-	    int candidate_key=moore_voting(arr,n);
-	    cout<<verify(arr,n,candidate_key)<<endl;
-	}
-	return 0;
+
+//  OR  //
+
+// Method 2: Using Sort
+// Approach: 
+// 1. Given the condition, if any element appear more than n/2 times is called Majority Element.
+// 2. Then we know our Mid Element means (n-1/2) is also our Majority Element if satisfied the given condition.
+// 3. Simply, return Mid Element.
+int majorityElement(vector <int> &nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[(n-1)/2];
 }
+
+//  OR  //
+
+// Method 3: Using hash -- Unordered_map
+int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map <int,int> ump;
+        for(int i=0;i<n;i++){
+            ump[nums[i]]++;
+            if(ump[nums[i]]>n/2)
+                return nums[i];
+        }
+        return -1;
+}
+
