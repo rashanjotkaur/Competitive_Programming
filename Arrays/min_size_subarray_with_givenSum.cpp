348348ed5fc92a5ef7172531449418de17c7bfb7
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-19 00:12:27 +0530

    Update min_size_subarray_with_givenSum.cpp

--- Arrays/min_size_subarray_with_givenSum.cpp
+++ Arrays/min_size_subarray_with_givenSum.cpp
@@ -3,6 +3,28 @@
 // Find the minimal length of a contiguous subarray for which the sum>=target. 
 // If there is no such subarray, return 0 instead.
 
+// Method 2 - O(n) | Sliding Window | Preferred
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int> &nums) {
+        int n=nums.size();
+        bool flag=false;
+        int start=0;
+        int len=n;
+        int currSum=0;
+        for(int i=0;i<n;i++){
+            currSum+=nums[i];
+            while(currSum>=target){
+                flag=true;
+                len=min(len,i-start+1);
+                currSum-=nums[start];
+                start++;
+            }
+        }
+        return flag?len:0;
+    }
+};
+
 
 // Method 1 - O(n^2) | Naive Solution | Time Limit Exceeded
 class Solution {
@@ -30,28 +52,3 @@ public:
     }
 };
 
-// Method 2 - O(n) | Sliding Window
-class Solution {
-public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int n=nums.size();
-        int len=n;
-        bool flag=false;
-        int currSum=0;
-        int start=0;
-        for(int i=0;i<n;i++){
-            currSum+=nums[i];
-            cout<<currSum<<" ";
-            while(currSum>=target){
-                if(currSum>=target){
-                    flag=true;
-                    len=min(len,i-start+1);
-                    cout<<" len: "<<len<<" currSum: "<<currSum<<endl;
-                }
-                currSum=currSum-nums[start];
-                start++;
-            }
-        }
-        return flag?len:0;
-    }
-};
