e2f72223eb69ce128ecb05fb6c40a481be67b81a
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-21 12:30:50 +0530

    Update 2d. count_subarrays_with_sum_equals_K.cpp

--- Arrays/Standard Problems_II/2d. count_subarrays_with_sum_equals_K.cpp
+++ Arrays/Standard Problems_II/2d. count_subarrays_with_sum_equals_K.cpp
@@ -1,6 +1,28 @@
 // Count Subarrays with Sum Equals K
 // https://leetcode.com/problems/subarray-sum-equals-k/description/
+// https://practice.geeksforgeeks.org/problems/subarray-range-with-given-sum0128/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
 
+// Method 1: Naive Approach
+// Time Complexity: O(N^2)
+int subArraySum(int nums[], int n, int k){
+        long long currSum=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            currSum=nums[i];
+            for(int j=i+1;j<=n;j++){
+                if(currSum==k)
+                    count++;
+                if(j==n)
+                    break;
+                currSum+=nums[j];
+            }
+        }
+        return count;
+}
+
+
+// Method 2: Using Hash - Preferred
+// Time Complexity: O(N)
 int subarraySum(vector <int> &nums, int k) {
         int n=nums.size();
         int count=0;
