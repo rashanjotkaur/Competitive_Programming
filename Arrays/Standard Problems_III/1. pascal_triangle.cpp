939d4d6cd260bd37e6aad889faf9cf0233c24e1f
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-21 17:16:18 +0530

    Update and rename Arrays/pascal_triangle.cpp to Arrays/Standard Problems_III/1. pascal_triangle.cpp

--- Arrays/Standard Problems_III/1. pascal_triangle.cpp
+++ Arrays/Standard Problems_III/1. pascal_triangle.cpp
@@ -1,8 +1,21 @@
 // https://leetcode.com/problems/pascals-triangle/
 
-class Solution {
-public:
-    vector<vector<int>> generate(int r) {
+// Method 1: Preferred
+vector <vector<int>> generate(int n) {
+        vector <vector<int>> dp(n);
+        for(int i=0;i<n;i++){
+            dp[i].resize(i+1);
+            dp[i][0]=1;
+            dp[i][i]=1;
+            for(int j=1;j<i;j++)
+                dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+        }
+        return dp;
+}
+
+
+// Method 2: 
+vector<vector<int>> generate(int r) {
         vector <vector<int>> res(r);
         if(r==1 || r==2){
             for(int i=0;i<r;i++){
@@ -34,5 +47,4 @@ public:
         //     cout<<endl;
         // }
         return res;
-    }
-};
+}
