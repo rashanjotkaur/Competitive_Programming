1b7eec915e563517eaba7ee162bf38d24f1a3a17
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-27 00:51:03 +0530

    Update 7b. print_nodes_at_dis_k_from_leaf_nodes.cpp

--- Trees/C/7b. print_nodes_at_dis_k_from_leaf_nodes.cpp
+++ Trees/C/7b. print_nodes_at_dis_k_from_leaf_nodes.cpp
@@ -1,10 +1,9 @@
 // Nodes at given distance in binary tree
 // https://www.geeksforgeeks.org/print-nodes-distance-k-given-node-binary-tree/
-// https://practice.geeksforgeeks.org/problems/nodes-at-given-distance-in-binary-tree/1
 
 // Func 1: 
 int printKDistantfromLeaf(Node* root, int k){
-	  vector <Node*> temp;
+    vector <Node*> temp;
     set <Node*> res;    // Using set, as to remove nodes that may get inserted more than once.
     find(root,k,temp,res);
     for(auto x:res)
