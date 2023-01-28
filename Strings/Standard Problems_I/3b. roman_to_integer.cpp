cb5e884845af3b5dd2cb0a5e0a1b1726cca9950b
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-28 22:14:54 +0530

    Update 3b. roman_to_integer.cpp

--- Strings/Standard Problems_I/3b. roman_to_integer.cpp
+++ Strings/Standard Problems_I/3b. roman_to_integer.cpp
@@ -16,7 +16,6 @@ int romanToInt(string s) {
         res=res+mapToInt(s[n-1]);
         return res;
 }
-
 // Func 2:
 int mapToInt(char ch){
         if(ch=='I')
@@ -36,3 +35,31 @@ int mapToInt(char ch){
         return -1;
 }
     
+
+
+// Method 2: Using Hashmap - Unordered Map
+// Func 1:
+int romanToInt(string s) {
+        int n=s.size();
+        int res=0;
+        unordered_map <char,int> ump;
+        mapRomanToInt(ump);
+        for(int i=0;i<n-1;i++){
+            if(ump[s[i]]<ump[s[i+1]])
+                res=res-ump[s[i]];
+            else
+                res=res+ump[s[i]];
+        }
+        res=res+ump[s[n-1]];
+        return res;
+}
+// Func 2:
+void mapRomanToInt(unordered_map <char,int> &ump){
+        ump['I']=1;
+        ump['V']=5;
+        ump['X']=10;
+        ump['L']=50;
+        ump['C']=100;
+        ump['D']=500;
+        ump['M']=1000;
+}
