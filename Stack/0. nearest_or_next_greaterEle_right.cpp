14b2412941992fd670ef1f7f2a5ae2cb13e6cfab
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-01-30 01:50:39 +0530

    Update and rename 1. nearest_greaterEle_right.cpp to 0. nearest_or_next_greaterEle_right.cpp

--- Stack/0. nearest_or_next_greaterEle_right.cpp
+++ Stack/0. nearest_or_next_greaterEle_right.cpp
@@ -1,3 +1,4 @@
+// Nearest or Next Greater Element
 // https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1/#
 
 vector<long long> nextLargerElement(vector<long long> vec, int n){
