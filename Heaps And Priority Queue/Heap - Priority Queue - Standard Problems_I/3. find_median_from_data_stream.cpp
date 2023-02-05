4bebdab1d61da4674228a87aa9b1d11c96e0a126
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-02-05 11:42:44 +0530

    Update 3. find_median_from_data_stream.cpp

--- Heaps And Priority Queue/Heap - Priority Queue - Standard Problems_I/3. find_median_from_data_stream.cpp
+++ Heaps And Priority Queue/Heap - Priority Queue - Standard Problems_I/3. find_median_from_data_stream.cpp
@@ -48,6 +48,43 @@ int main() {
     }
 	return 0;
 }
+
+
+// OR
+
+class Solution{
+    public:
+    //Function to insert heap.
+    priority_queue <int> pq1;
+    priority_queue <int,vector<int>,greater<int>> pq2;
+    void insertHeap(int &x){
+        if(pq1.empty() || pq1.top()>x)
+            pq1.push(x);
+        else 
+            pq2.push(x);
+        balanceHeaps();
+    }
+    
+    //Function to balance heaps.
+    void balanceHeaps(){
+        if(pq1.size()>pq2.size()+1){
+            pq2.push(pq1.top());
+            pq1.pop();
+        }
+        else if(pq2.size()>pq1.size()+1){
+            pq1.push(pq2.top());
+            pq2.pop();
+        }
+    }
+    
+    //Function to return Median.
+    double getMedian(){
+        if(pq1.size()==pq2.size())
+            return (double)(pq1.top()+pq2.top())/2.0;
+        else
+            return pq1.size()>pq2.size()?pq1.top():pq2.top();
+    }
+};
     
 
 // Method 1: Using Insertion Sort Algo
@@ -59,5 +96,37 @@ int main() {
 //    the first i elements of the array are sorted. The insertion sort doesn’t depend on future data to sort data input till that point. 
 // 3. In other words, insertion sort considers data sorted so far while inserting the next element. 
 // 4. This is the key part of insertion sort that makes it an online algorithm.
+
     
-    
+#include <bits/stdc++.h>
+using namespace std;
+
+double insertionSort(vector <int> &vec, int x){
+    vec.push_back(x);
+    int n=vec.size();
+    int key=vec[n-1];
+    int i=n-2;
+    while(i>=0 && vec[i]>key){
+        vec[i+1]=vec[i];
+        i--;
+    }
+    vec[i+1]=key;
+    if(n%2)
+        return vec[n/2];
+    return (double)(vec[(n/2)-1]+vec[n/2])/2.0;
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n;
+	    vector <int> vec;
+	    for(int i=0;i<n;i++){
+	        cin>>x;
+	        cout<<insertionSort(vec,x)<<" ";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}    
