a100c2955463126f58b22190f0789d86c8bd399a
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-02-11 01:02:13 +0530

    Create 3. max_sum_without_adjacent.cpp

---
+++ Dynamic_Programming/Linear DP/3. max_sum_without_adjacent.cpp
@@ -0,0 +1,75 @@
+// Max Sum without Adjacents
+// Problem Link: https://practice.geeksforgeeks.org/problems/7a33c749a79327b2889d420dd80342fff33aac6d/1
+
+// Method 3: Tabulation - DP
+int findMaxSum(int *arr, int n) {
+	    vector <int> dp(n,0);
+	    dp[0]=arr[0];
+	    dp[1]=max(arr[0],arr[1]);
+	    for(int i=2;i<n;i++){
+	        int pick=arr[i]+dp[i-2];
+	        int noPick=0+dp[i-1];
+	        dp[i]=max(pick,noPick);
+	    }
+	    return dp[n-1];
+}
+
+// Method 2: Memoization - While doing recursion, we are storing the values
+int findMax(int arr[], int idx, vector <int> &dp){
+	    if(idx==0)
+	        return arr[0];
+	    if(idx<0)
+	        return 0;
+	    
+	    int pick,noPick;
+	    if(idx>=2 && dp[idx-2]!=-1)
+	        pick=arr[idx]+dp[idx-2];
+	    else
+	        pick=arr[idx]+findMax(arr,idx-2,dp);
+	    
+	    if(idx>=1 && dp[idx-1]!=-1)
+	        noPick=dp[idx-1];
+	    else
+	        noPick=findMax(arr,idx-1,dp);
+	    return dp[idx]=max(pick,noPick);
+}
+int findMaxSum(int *arr, int n) {
+	    vector <int> dp(n,-1);
+	    return findMax(arr,n-1,dp);
+}
+
+// OR 
+
+int findMax(int arr[], int idx, vector <int> &dp){
+	    if(idx==0)
+	        return arr[0];
+	    if(idx<0)
+	        return 0;
+	    
+	    if(dp[idx]!=-1)
+	        return dp[idx];
+	        
+	    int pick=arr[idx]+findMax(arr,idx-2,dp);
+	    int noPick=findMax(arr,idx-1,dp);
+	    
+	    return dp[idx]=max(pick,noPick);
+}
+int findMaxSum(int *arr, int n) {
+    	vector <int> dp(n,-1);
+    	return findMax(arr,n-1,dp);
+}
+
+
+// Method 1: Recursion - TLE occurs after 10 cases
+int findMax(int arr[], int idx){
+	    if(idx==0)
+	        return arr[0];
+	    if(idx<0)
+	        return 0;
+	    int pick=arr[idx]+findMax(arr,idx-2);
+	    int noPick=findMax(arr,idx-1);
+	    return max(pick,noPick);
+}
+int findMaxSum(int *arr, int n) {
+	    return findMax(arr,n-1);
+}
