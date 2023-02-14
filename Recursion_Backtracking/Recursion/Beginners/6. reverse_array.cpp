789a696d95be0b1c4b8fb553f11d1944a64c29dd
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-02-14 15:50:50 +0530

    Update 6. reverse_array.cpp

--- Recursion_Backtracking/Recursion/Beginners/6. reverse_array.cpp
+++ Recursion_Backtracking/Recursion/Beginners/6. reverse_array.cpp
@@ -12,3 +12,18 @@ string reverseWord(string s){
     reverse(s,0,n);
     return s;
 }
+
+
+// OR 
+// Using Two Pointers
+void reverse(string &s, int l, int r){
+    if(l>=r)
+        return ;
+    swap(s[l],s[r]);
+    reverse(s,l+1,r-1);
+}
+string reverseWord(string s){
+    int n=s.size();
+    reverse(s,0,n-1);
+    return s;
+}
