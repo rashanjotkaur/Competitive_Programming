6b2482985dd53c589d1b823cf52943a4bd30244f
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-02-19 22:30:02 +0530

    Update 6a. combination_sum_I.cpp

--- Recursion_Backtracking/Recursion/Multiple Recursion Calls/6a. combination_sum_I.cpp
+++ Recursion_Backtracking/Recursion/Multiple Recursion Calls/6a. combination_sum_I.cpp
@@ -3,6 +3,48 @@
 // Problem Link: https://practice.geeksforgeeks.org/problems/combination-sum-1587115620/1
 // https://www.geeksforgeeks.org/combinational-sum/
 
+// Method 3: Best Approach 
+// 1. Since, we can use each element any number of times. 
+// 2. So, if duplicates are there in the array, then we are checking for the same combination of sequences again and again.
+// 3. Duplicate Sequences are getting created.
+// 4. Hence, first remove the duplicates from the array.
+// 5. For removing duplicates - Sort the array first and then use sliding window for comparing.
+void combinations(vector <int> nums, int i, int n, int target, vector <int> temp, set<vector<int>> &s){
+        if(target<0)
+            return;
+        if(i==n){
+            if(target==0)
+                s.insert(temp);
+            return ;
+        }
+        temp.push_back(nums[i]);
+        combinations(nums,i,n,target-nums[i],temp,s);
+        temp.pop_back();
+        combinations(nums,i+1,n,target,temp,s);
+}
+vector <vector<int> > combinationSum(vector <int> &nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int cmp=nums[0];
+        int idx=1;
+        for(int i=1;i<n;i++){
+            if(cmp!=nums[i]){
+                nums[idx]=nums[i];
+                cmp=nums[idx];
+                idx++;
+            }
+        }
+        vector <vector<int>> res;
+        set <vector<int>> s;
+        vector <int> temp;
+        combinations(nums,0,idx,target,temp,s);
+        for(auto x:s){
+            temp=x;
+            res.push_back(temp);
+        }
+        return res;
+}
+
 
 // Method 2: Same as Previous - Just Subtracting from the target
 void combinations(vector <int> nums, int i, int n, int target, vector <int> temp, vector<vector<int>> &res){
