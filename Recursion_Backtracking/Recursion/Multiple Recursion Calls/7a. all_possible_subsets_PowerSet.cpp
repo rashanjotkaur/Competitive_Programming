26a8d9d18fa911bc2c97f4a6a05ac840279b56bc
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-02-28 12:23:51 +0530

    Update 7a. all_possible_subsets_PowerSet.cpp

--- Recursion_Backtracking/Recursion/Multiple Recursion Calls/7a. all_possible_subsets_PowerSet.cpp
+++ Recursion_Backtracking/Recursion/Multiple Recursion Calls/7a. all_possible_subsets_PowerSet.cpp
@@ -1,8 +1,10 @@
-// Find all possible Subsets 
-// Find the Power Set
+// Find all possible Subsets - Find the Power Set
 // Problem Link: https://leetcode.com/problems/subsets/
 
 // Method 2: Using Iterative Approach 
+// Advantage of Using Iterative Approach: 
+// 1. If the array is sorted then, using this approach subsets will stored in vector in sorted manner.
+// 2. We don't have to sort the res - vector.
 // Func 1:
 vector<vector<int>> subsets(vector <int> &nums) {
         int n=nums.size();
@@ -16,7 +18,6 @@ void findPowerSet(vector <int> ip, int i, int n, vector <int> op, vector <vector
         res.push_back(op);
         if(i==n)
             return;
-        
         for(int j=i;j<n;j++){
             op.push_back(ip[j]);
             findPowerSet(ip,j+1,n,op,res);
@@ -27,7 +28,7 @@ void findPowerSet(vector <int> ip, int i, int n, vector <int> op, vector <vector
 
 // Method 1: Using Recursion - By Picking and Not Picking
 // Func 1:
-vector<vector<int>> subsets(vector <int> &nums) {
+vector <vector<int>> subsets(vector <int> &nums) {
         int n=nums.size();
         vector <vector<int>> res;
         vector <int> op;
@@ -40,7 +41,6 @@ void findPowerSet(vector <int> ip, int i, int n, vector <int> op, vector <vector
             res.push_back(op);
             return;
         }
-    
         op.push_back(ip[i]);
         findPowerSet(ip,i+1,n,op,res);
         op.pop_back();
