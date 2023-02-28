cf7902e07a60ee37998d0c3fb04fc7fa366086f1
rashan_dhillon <40189953+rashanjotkaur@users.noreply.github.com>
2023-02-28 16:56:58 +0530

    Create 9. print_all_permutations.cpp

---
+++ Recursion_Backtracking/Recursion/9. print_all_permutations.cpp
@@ -0,0 +1,56 @@
+// Print all Permutations in String
+// Problem Link: https://practice.geeksforgeeks.org/problems/permutations-of-a-given-string2041/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+// Method 2: Without Extra Space: By using swaping
+// Func 1:
+vector <string> find_permutation(string s){
+		    set <string> temp;
+		    allPermutations(s,0,s.size(),temp);
+		    vector <string> res;
+		    for(auto x:temp)
+		        res.push_back(x);
+		    return res;
+}
+// Func 2:
+void allPermutations(string ip, int i, int n, set <string> &res){
+	        if(i==n){
+	            res.insert(ip);
+	            return;
+	        }
+	        for(int j=i;j<n;j++){
+	            swap(ip[j],ip[i]);
+	            allPermutations(ip,i+1,n,res);
+	            swap(ip[j],ip[i]);
+	        }
+}
+
+
+// Method 1: Using Extra Space: Similar to Iterative approach of finding all subsets.
+// Func 1:
+vector <string> find_permutation(string s){
+		    set <string> temp;
+		    unordered_map <int,bool> ump;
+		    string op="";
+		    allPermutations(s,op,temp,ump);
+		    vector <string> res;
+		    for(auto x:temp)
+		        res.push_back(x);
+		    return res;
+}
+// Func 2:
+void allPermutations(string ip, string op, set <string> &temp, unordered_map <int,bool> ump){
+          if(ip.size()==op.size()){
+	            temp.insert(op);
+	            return;
+	        }
+	        for(int i=0;i<ip.size();i++){
+	            if(!ump[i]){
+	                ump[i]=true;
+	                op.push_back(ip[i]);
+	                allPermutations(ip,op,temp,ump);
+	                ump[i]=false;
+	                op.pop_back();
+	            }
+	        }
+}
+		
